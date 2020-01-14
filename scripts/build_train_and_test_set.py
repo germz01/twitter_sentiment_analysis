fe7288e6e09624aa779d92acfd31ac15c3b44413
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-01-14 10:08:14 +0100

    Updated script for training and testing set building

--- scripts/build_train_and_test_set.py
+++ scripts/build_train_and_test_set.py
@@ -17,23 +17,32 @@ def clean_mentions_tags_and_links(tweet):
 path_to_ds = '../data/DOWNLOAD/Subtask_A/'
 dirs = [x for x in os.walk(path_to_ds)][0][1]
 
-datasets = []
+datasets_tr, datasets_ts = [], []
 
 for year in dirs:
     name = 'twitter_{}_train_A.txt'.format(year)
     ds = pd.read_csv(path_to_ds + year + '/' + name, sep='\t',
                      names=['id', 'label', 'tweet'])
-    datasets.append(ds)
+    datasets_tr.append(ds)
 
-f_ts = pd.concat(datasets, ignore_index=True)
-f_ts.drop(f_ts[f_ts.tweet == 'Not Available'].index, inplace=True)
-f_ts.drop_duplicates(subset='tweet', inplace=True)
-f_ts.drop(labels='id', axis=1, inplace=True)
+    ds = pd.read_csv(path_to_ds + year + '/' + name.replace('train', 'test'),
+                     sep='\t', names=['id', 'label', 'tweet'])
+    datasets_ts.append(ds)
+
+for i, datasets in enumerate([datasets_tr, datasets_ts]):
+    name = 'training_set.csv'
+
+    f_ds = pd.concat(datasets, ignore_index=True)
+    f_ds.drop(f_ds[f_ds.tweet == 'Not Available'].index, inplace=True)
+    f_ds.drop_duplicates(subset='tweet', inplace=True)
+    f_ds.drop(labels='id', axis=1, inplace=True)
 
 # REMOVE TAGS, MENTIONS AND LINKS FROM THE TWEETS #############################
 
-f_ts.tweet = f_ts.tweet.apply(clean_mentions_tags_and_links)
+    f_ds.tweet = f_ds.tweet.apply(clean_mentions_tags_and_links)
 
 # SAVE THE FINAL TRAINING DATASET AS A CSV FILE ###############################
 
-f_ts.to_csv(path_or_buf=path_to_ds + 'training_set.csv', index=False)
+    f_ds.to_csv(path_or_buf=path_to_ds + name, index=False)
+
+    name = 'test_set.csv'
