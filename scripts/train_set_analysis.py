7dd1510869f87cc71bec8233b51121a27ee15107
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-01-15 17:17:39 +0100

    Updated tag analysis

--- scripts/train_set_analysis.py
+++ scripts/train_set_analysis.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from collections import Counter
+from collections import defaultdict
+from nltk import word_tokenize
+from nltk import pos_tag
 
 """
 VISUALIZZAZIONI: DISTRIBUZIONE DELLA FREQUENZA DELLE PAROLE PER VEDERE SE
@@ -19,6 +22,8 @@ VISUALIZZAZIONI: DISTRIBUZIONE DELLA FREQUENZA DELLE PAROLE PER VEDERE SE
 
 ds = pd.read_csv('../data/train.csv')
 
+# DISTRIBUTION OF THE WORD FREQUENCIES ########################################
+
 c = Counter()
 
 for tweet in ds.tweet:
@@ -33,4 +38,47 @@ plt.xlabel('Words')
 plt.ylabel('Frequencies')
 plt.title('Distribution of the word frequencies')
 plt.savefig('../images/word_frequencies.png')
+plt.close()
+
+# TAG DISTRIBUTION FOR POSITIVE TWEETS VS NEGATIVE TWEETS #####################
+
+positive_sample = ds[ds.label == 'positive'].sample(500, random_state=42)
+negative_sample = ds[ds.label == 'negative'].sample(500, random_state=42)
+samples = {'positive': positive_sample, 'negative': negative_sample}
+
+tags_counter = defaultdict(list)
+
+for label in ['positive', 'negative']:
+    samples[label]['tags'] = [pos_tag(word_tokenize(t))
+                              for t in samples[label].tweet]
+
+    for tags in samples[label].tags:
+        for tag in tags:
+            tags_counter[label].append(tag[1])
+
+tags_counter = {'positive': Counter(tags_counter['positive']),
+                'negative': Counter(tags_counter['negative'])}
+tags_polarity = dict()
+
+for tag in set(list(tags_counter['positive'].keys()) +
+               list(tags_counter['negative'].keys())):
+    num = tags_counter['positive'][tag] - tags_counter['negative'][tag]
+    den = tags_counter['positive'][tag] + tags_counter['negative'][tag]
+    tags_polarity[num / den] = tag
+
+bars = plt.bar([2 * i for i in range(len(list(tags_polarity.keys())))],
+               sorted(list(tags_polarity.keys())), zorder=5)
+plt.grid(axis='y', zorder=3)
+
+sk = sorted(list(tags_polarity.keys()))
+
+for i, rect in enumerate(bars):
+    plt.text(rect.get_x() + rect.get_width() / 2.0, rect.get_height(),
+             tags_polarity[sk[i]], ha='center', va='bottom')
+
+plt.xticks([])
+plt.xlabel('Tags')
+plt.ylabel('Polarity')
+plt.title('Tag distribution: positive tweets VS negative tweets')
+plt.savefig('../images/tag_distribution_positive_vs_negative.png')
 plt.close()
