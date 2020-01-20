fd3c300744798a833c730ced1f5821a90d7ae160
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-01-20 12:22:26 +0100

    Updated SVM classification script and saved results

--- scripts/svm_classification.py
+++ scripts/svm_classification.py
@@ -18,14 +18,35 @@ def remove_alpha(tweet):
                     if token.isalpha()])
 
 
+c_type = input('CLASSIFICATION TYPE[SO, PN, A]: ')
+
 trainset = pd.read_csv('../data/train.csv')
 testset = pd.read_csv('../data/test.csv')
 
-X_train = trainset.tweet.values
-y_train = trainset.label.map({'positive': 1, 'negative': 2, 'neutral': 3}).\
-    values
-X_test = testset.tweet.values
-y_test = testset.label.map({'positive': 1, 'negative': 2, 'neutral': 3}).values
+X_train, X_test, y_train, y_test = None, None, None, None
+
+if c_type == 'SO':
+    X_train = trainset.tweet.values
+    X_test = testset.tweet.values
+    y_train = trainset.label.\
+        map({'positive': 1, 'negative': 1, 'neutral': 2}).values
+    y_test = testset.label.\
+        map({'positive': 1, 'negative': 1, 'neutral': 2}).values
+elif c_type == 'PN':
+    X_train = trainset[trainset.label != 'neutral'].tweet.values
+    X_test = testset[testset.label != 'neutral'].tweet.values
+    y_train = trainset[trainset.label != 'neutral'].label.\
+        map({'positive': 1, 'negative': 2}).values
+    y_test = testset[testset.label != 'neutral'].label.\
+        map({'positive': 1, 'negative': 2}).values
+else:
+    X_train = trainset.tweet.values
+    X_test = testset.tweet.values
+    y_train = trainset.label.\
+        map({'positive': 1, 'negative': 2, 'neutral': 3}).values
+    y_test = testset.label.\
+        map({'positive': 1, 'negative': 2, 'neutral': 3}).values
+
 
 tokenizer = TweetTokenizer(strip_handles=True, preserve_case=False,
                            reduce_len=True)
@@ -41,11 +62,12 @@ learner = LinearSVC()
 pipe = Pipeline([('vect', vectorizer), ('trans', transformer),
                 ('select', selectioner), ('svm', learner)])
 
-parameters = {'trans__sublinear_tf': (False, True),
+parameters = {'vect__ngram_range': ((1, 1), (1, 2)),
+              'trans__sublinear_tf': (False, True),
               'select__k': (1000, 2000, 5000),
               'svm__C': tuple(np.linspace(0.1, 1., 3))}
 
-classifier = GridSearchCV(pipe, parameters, n_jobs=-1, verbose=4,
+classifier = GridSearchCV(pipe, parameters, n_jobs=-1, verbose=3,
                           scoring='f1_macro', cv=3)
 classifier.fit(X_train, y_train)
 
@@ -57,4 +79,5 @@ for param_name in sorted(parameters.keys()):
 
 predictions = classifier.predict(X_test)
 
-print(classification_report(y_test, predictions))
+pd.DataFrame(classification_report(y_test, predictions, output_dict=True)).\
+    transpose().to_csv('./classification_report_svm_{}.csv'.format(c_type))
