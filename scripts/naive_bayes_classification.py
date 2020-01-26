f71499fd24f2fdc64c817883b286f691e4ece8d0
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2020-01-26 12:51:57 +0100

    Update

--- scripts/naive_bayes_classification.py
+++ scripts/naive_bayes_classification.py
@@ -9,6 +9,7 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
@@ -21,31 +22,19 @@ def remove_alpha(tweet):
 c_type = input('CLASSIFICATION TYPE[SO, PN, A]: ')
 
 trainset = pd.read_csv('../data/train.csv')
-testset = pd.read_csv('../data/test.csv')
-
-X_train, X_test, y_train, y_test = None, None, None, None
 
 if c_type == 'SO':
-    X_train = trainset.tweet.values
-    X_test = testset.tweet.values
-    y_train = trainset.label.\
-        map({'positive': 1, 'negative': 1, 'neutral': 2}).values
-    y_test = testset.label.\
-        map({'positive': 1, 'negative': 1, 'neutral': 2}).values
+    trainset.label = trainset.label.\
+      map({'positive': 1, 'negative': 1, 'neutral': 2})
 elif c_type == 'PN':
-    X_train = trainset[trainset.label != 'neutral'].tweet.values
-    X_test = testset[testset.label != 'neutral'].tweet.values
-    y_train = trainset[trainset.label != 'neutral'].label.\
-        map({'positive': 1, 'negative': 2}).values
-    y_test = testset[testset.label != 'neutral'].label.\
-        map({'positive': 1, 'negative': 2}).values
+    trainset = trainset[trainset.label != 'neutral']
+    trainset.label = trainset.label.map({'positive': 1, 'negative': 2})
 else:
-    X_train = trainset.tweet.values
-    X_test = testset.tweet.values
-    y_train = trainset.label.\
-        map({'positive': 1, 'negative': 2, 'neutral': 3}).values
-    y_test = testset.label.\
-        map({'positive': 1, 'negative': 2, 'neutral': 3}).values
+    trainset.label = trainset.label.\
+      map({'positive': 1, 'negative': 2, 'neutral': 3})
+
+X_train, X_test, y_train, y_test = \
+  train_test_split(trainset.tweet, trainset.label, test_size=0.2)
 
 
 tokenizer = TweetTokenizer(strip_handles=True, preserve_case=False,
@@ -69,7 +58,7 @@ parameters = {'vect__ngram_range': ((1, 1), (1, 2)),
 
 classifier = GridSearchCV(pipe, parameters, n_jobs=-1, verbose=3,
                           scoring='f1_macro', cv=3)
-classifier.fit(X_train, y_train)
+classifier.fit(X_train.values, y_train.values)
 
 best_parameters = classifier.best_estimator_.get_params()
 for param_name in sorted(parameters.keys()):
@@ -77,7 +66,7 @@ for param_name in sorted(parameters.keys()):
 
 # PREDICTION ##################################################################
 
-predictions = classifier.predict(X_test)
+predictions = classifier.predict(X_test.values)
 
 pd.DataFrame(classification_report(y_test, predictions, output_dict=True)).\
     transpose().to_csv('../results/classification_report_bayes_{}.csv'.
